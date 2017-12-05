fb2c57a996ae2149f45b0f77f29145b22200df3b
guoqi47 <guoqi47@163.com>
2017-12-05 21:59:39 +0800

    实验室任务完成

--- HS300KDJ_CSV.py
+++ HS300KDJ_CSV.py
@@ -3,27 +3,81 @@ import os
 import csv
 from HS300KDJ_CSV_culKDJ import cul_KDJ
 
-path = "D:/PythonCode/TimeSeries/300data_day1" #文件夹目录  
-files= os.listdir(path) #得到文件夹下的所有文件名称  
-s = []  
-for file in files: #遍历文件夹  
-    a = file.split('.')
-    if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开  
-        filePath = path+"/"+file #打开文件  
-#        open(filePath).readlines()
-        with open(filePath) as f:
-            f_csv = csv.reader(f)
-            headers = next(f_csv)
-            for row in f_csv:
-                s.append(cul_KDJ(a[0],row[0]))
-        
+def culFlag(K,D,J,index,K_value,D_value,J_value,CBigThanMA5): #传入所有KDJlist，index，当日KDJ,收盘价是否大于MA5
+    threshold=8 #判断金死叉的阈值
+    #买入卖出判断，-1看跌，1看涨，0持有
+    if index<len(K)-1:
+        if abs(J_value-D_value)<threshold: #近金叉或者死叉
+            if J_value-D_value>0: 
+                if J[index-1]-D[index-1]>J_value-D_value:
+                    return -1
+                if J[index-1]-D[index-1]<threshold and CBigThanMA5:
+                    return 1
+                else:
+                    return 0   
+            elif J_value<D_value and D[index-1]-J[index-1]>threshold and CBigThanMA5: #即将金叉
+                return 1
+                        
+            else:
+                return -1
+                        
+        if J_value>D_value and CBigThanMA5: #要涨，K线总是在中间,故简化
+            return 1
+                        
+        elif J_value<D_value: #要跌
+            return -1
+                    
+        else:  #持有
+            return 0
+    return 0
+  
+
+if __name__=="__main__":
+    start='2015-10-31'
+    end='2017-10-31'
+    path = "D:/PythonCode/TimeSeries/300data_day" #文件夹目录  
+    files= os.listdir(path) #得到文件夹下的所有文件名称  
+    
+    for file in files: #遍历文件夹  
+        s = []  
+        a = file.split('.')
+        if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开  
+            filePath = path+"/"+file #打开文件  
+            K,D,J,dateList,CBigThanMA5 = cul_KDJ(a[0])
+            with open(filePath) as f:
+                f_csv = csv.reader(f)
+                headers = next(f_csv)
+                for row in f_csv:
+                    #计算KDJ
+                    if row[0] != '2016-11-01' and row[0] != '2016-10-31':
+                        index = dateList.index(row[0])
+                        K_value = K[index]
+                        D_value = D[index]
+                        J_value = J[index]
+                        m = CBigThanMA5[index] #收盘价是否大于五日均线
+                        d = {'K':K_value,'D':D_value,'J':J_value}
+                        row.append(d)
+                        row.append(culFlag(K,D,J,index,K_value,D_value,J_value,m))
+                        s.append(row)
+                    else:
+                        d = {'K':0,'D':0,'J':0}
+                        row.append(d)
+                        row.append(0)
+                        s.append(row)
+            #重写文件
+            with open(filePath,'w',newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['date', 'open', 'high','close', 'low','volume', 'amount','KDJ_value', 'flag'])
+                for item in s:
+                     writer.writerow([item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8]])
             
                 
-                   
-     
+                    
+                       
+         
+                  
               
-          
-                   
-           
-                
-    
+                       
+               
+                    
+        
--- HS300KDJ_CSV_culKDJ.py
+++ HS300KDJ_CSV_culKDJ.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+#from datetime import datetime
 import tushare as ts
 
-def cul_KDJ(DM,date,days=9):
+def cul_KDJ(DM,days=9):
     days -=1
     
     C,L,H=[],[],[] #周期内每日收盘价,每日最低价,每日最高价
     K,D,J=[],[],[]
     RSV=[] #股票开盘日起每日RSV
-    threshold=8 #判断金死叉的阈值
+    MA5=[0,0,0,0] #五日均线，用于判断flag
+    CBigThanMA5=[]
     data = ts.get_hist_data(DM)
     dateList = [i for i in data.index][::-1] #日期list
     C = [i for i in data['close'][::-1]] #第n日收盘价
@@ -22,6 +23,14 @@ def cul_KDJ(DM,date,days=9):
         else:
             t=i-days
         RSV.append((C[i]-min(L[t:i+1]))/(max(H[t:i+1])-min(L[t:i+1]))*100)
+    #计算五日均线是否大于收盘价
+    for i in range(4,len(C)):
+        m=0
+        for j in range(5):
+            m += C[i-j]
+        MA5.append(m/5)
+    for i in range(len(MA5)):
+        CBigThanMA5.append(MA5[i]<C[i])
     
     for i in range(3):
         K.append(100)
@@ -33,67 +42,23 @@ def cul_KDJ(DM,date,days=9):
         J.append(K[i]*3 - 2*D[i])
     
     #要获取的数据在Klist的索引
-    index = dateList.index(str(datetime.strptime(date, "%Y-%m-%d"))[:10]) 
-    print('股票代码:',DM,'日期：',date)
-    K1=K[index]
-    D1=D[index]
-    J1=J[index]
-    print('K:',round(K1,2))
-    print('D:',round(D1,2))
-    print('J:',round(J1,2))
+#    index = dateList.index(str(datetime.strptime(date, "%Y-%m-%d"))[:10]) 
+#    print('股票代码:',DM,'日期：',date)
+#    K1=round(K[index],2)
+#    D1=round(D[index],2)
+#    J1=round(J[index],2)
+#    print('K:',K1)
+#    print('D:',D1)
+#    print('J:',J1)
+    return K,D,J,dateList,CBigThanMA5
+
     
-    #买入卖出判断，-1看跌，1看涨，0持有
-    if index<len(C)-1:
-        if abs(J1-D1)<threshold: #近金叉或者死叉
-            if J1-D1>0: 
-                if J[index-1]-D[index-1]>J1-D1:
-                    if C[index+1]<C[index]:
-                        return -1,1
-                    else:
-                        return -1,0
-                if J[index-1]-D[index-1]<threshold:
-                    if C[index+1]>C[index]:
-                        return 1,1
-                    else:
-                        return 1,0
-                else:
-                    return 0,0   
-            elif J1<D1 and D[index-1]-J[index-1]>threshold: #即将金叉
-                if C[index+1]>C[index]:
-                    return 1,1
-                else:
-                    return 1,0
-                        
-            else:
-                print(-1)
-                if C[index+1]<C[index]:
-                    return -1,1
-                else:
-                    return -1,0
-                        
-        if J1>D1: #要涨，K线总是在中间,故简化
-            print(1)
-            if C[index+1]>C[index]:
-                return 1,1
-            else:
-                return 1,0
-                        
-        elif J1<D1: #要跌
-            print(-1)
-            if C[index+1]<C[index]:
-                return -1,1
-            else:
-                return -1,0
-                    
-        else:  #持有
-            print(0)
-            return 0,0
-    return 0,0
-  
     
 if __name__=="__main__":
 #    #cul_KDJ(DM,date,days,num)
 #    #DM:股票代码 date:目标日期 days:周期 num:KDJ均为100的天数
-    cul_KDJ('002253','2017-09-21',9)
+    a,s,d,f,g=cul_KDJ('002253',9)
+    if g[4]:
+        print(1)
 
     
